cbb72c0df1ae6b965c56877f562e473a2d699a5e
Davide Setti <davide.setti@gmail.com>
2011-09-30 14:24:43 +0200

    add exception handler in project_worker()

--- down.py
+++ down.py
@@ -127,6 +127,8 @@ def project_worker():
                               partners, contact, reference)
 
             out_queue.put(project)
+        except Exception, e:
+            logging.exception(e)
         finally:
             project_queue.task_done()
 
@@ -181,8 +183,6 @@ def unescape(text):
 
 if __name__ == "__main__":
     project_cache = shelve.open("project_cache.shelve")
-    print project_cache.keys()
-
 
     q = JoinableQueue()
     project_queue = JoinableQueue()
@@ -201,6 +201,7 @@ if __name__ == "__main__":
         q.join()  # block until all tasks are done
         project_queue.join()
     except KeyboardInterrupt:
+        logging.info('CTRL-C: save before exit')
         project_cache.close()
         raise
     project_cache.close()

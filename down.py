afda72176066622d7325c4c192d54aef45c8806e
Davide Setti <davide.setti@gmail.com>
2011-09-30 10:11:04 +0200

    iterate over themes

--- down.py
+++ down.py
@@ -15,14 +15,31 @@ logging.basicConfig(level=logging.DEBUG)
 NUM_THEME_WORKER_THREADS = 4
 NUM_PROJECT_WORKER_THREADS = 20
 
+THEME_URL = "http://cordis.europa.eu/fetch?CALLER=FP7_PROJ_EN&QM_EP_PGA_A=%(theme)s"
+
+def get_projects(doc):
+    for result in doc.findAll(title=u"Project acronym"):
+        a = result.a
+        link = "http://cordis.europa.eu" + dict(a.attrs)['href'][2:]
+        yield link
+
 def theme_worker():
     logging.info('START THEME WORKER')
     while True:
-        item = q.get()
-        logging.info('THEME: %s', repr(item))
+        theme = q.get()
+        logging.info('THEME: %s', repr(theme))
+
         try:
-            # do_work(item)
-            project_queue.put('p')
+            url = THEME_URL % {'theme': theme}
+            r = requests.get(url)
+            if r.status_code != 200:
+                logging.error("Request failed for url: %s", url)
+                continue
+            doc = BeautifulSoup(r.content)
+            for proj in get_projects(doc):
+                project_queue.put((theme, proj))
+        except:
+            logging.error("THEME_WORKER: Error for url: %s", url)
         finally:
             q.task_done()
 
